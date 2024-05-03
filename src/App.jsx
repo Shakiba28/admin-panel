761ca925e884c895bcb055d73281bd0bf7db325d
shakiba <bshkyba430@gmail.com>
2024-05-03 18:18:12 +0330

    add project in github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Dashboard from "./component/Dashboard"
+import Navbar from "./component/layout/Navbar"
+import Sidebar from "./component/layout/Sidebar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Dashboard.jsx
@@ -0,0 +1,24 @@
+
+export default function Dashboard() {
+
+    return (
+        <>
+
+
+
+
+
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 dark:border-gray-700 mt-14">
+
+                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">
+                            Welcome To Dashboard
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
